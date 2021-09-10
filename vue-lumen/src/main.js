d3b01aacf14f8feecac292a7e12484529c857f3c
jmontefalcon <john.montefalcon@veritaspay.com>
2021-09-10 16:41:01 +0800

    WIP

--- vue-lumen/src/main.js
+++ vue-lumen/src/main.js
@@ -1,9 +1,14 @@
 import { createApp } from 'vue';
-import App from './App.vue';
-
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+
+import App from './App.vue';
+import store from './store';
 import router from "./router/index";
 
 
-createApp(App).use(router).use(VueAxios, axios).mount('#app')
+createApp(App)
+.use(router)
+.use(store)
+.use(VueAxios, axios)
+.mount('#app')
